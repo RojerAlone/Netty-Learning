628ecc8c577552001784bce895bdebadaf435144
zhangrenjie <zhangrenjie@xiaomi.com>
2018-04-12 20:22:07 +0800

    add echo server

---
+++ netty-practice/src/main/java/cn/alone/demo/Netty4UserGuideBaseWaylau/echo/EchoServer.java
@@ -0,0 +1,56 @@
+package cn.alone.demo.Netty4UserGuideBaseWaylau.echo;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * Created by RojerAlone on 2018-04-12
+ */
+public class EchoServer {
+
+
+    private static final int PORT = 9999;
+
+    private int port;
+
+    public EchoServer(int port) {
+        this.port = port;
+    }
+
+    public void run() throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true)
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new EchoServerHandler());
+                        }
+                    });
+            ChannelFuture future = bootstrap.bind(port).sync();
+            future.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+
+    /**
+     * 启动后在命令行执行 telnet localhost PORT 即可，输入信息可以得到刚才输入的信息
+     */
+    public static void main(String[] args) throws Exception {
+        new EchoServer(PORT).run();
+    }
+
+}
---
+++ netty-practice/src/main/java/cn/alone/demo/Netty4UserGuideBaseWaylau/echo/EchoServerHandler.java
@@ -0,0 +1,23 @@
+package cn.alone.demo.Netty4UserGuideBaseWaylau.echo;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * Created by RojerAlone on 2018-04-12
+ */
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf) msg;
+        // 相当于 write 和 flush 的结合
+        ctx.writeAndFlush(buf);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
